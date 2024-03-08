a0c9a363308a11ff1d58cea9c786ea4e98ed2f6b
edwingd18 <edwin.guerrero@uao.edu.co>
2024-03-08 01:17:27 -0500

    Instalar Flowbite React

--- EstructuraCine/tailwind.config.js
+++ EstructuraCine/tailwind.config.js
@@ -1,8 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}',
+  'node_modules/flowbite-react/lib/esm/**/*.js'],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    require('flowbite/plugin'),
+  ],
 };
