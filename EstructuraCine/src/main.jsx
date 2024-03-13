5def8d0d8c25949f3478962f25b6e35b15e3663d
edwingd18 <edwin.guerrero@uao.edu.co>
2024-03-13 07:35:05 -0500

    Update main.jsx

--- EstructuraCine/src/main.jsx
+++ EstructuraCine/src/main.jsx
@@ -3,13 +3,11 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import  Button from './button.jsx'
-import Sidebar from './Components/Sidebar.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
     <Button />
-    <Sidebar />
 
   </React.StrictMode>,
 )
