664565fa5ec54a6606284c728a93704723f5abd4
edwingd18 <124949020+edwingd18@users.noreply.github.com>
2024-03-13 15:52:06 -0500

    tratando de reinstalar tailwind

--- EstructuraCine/tailwind.config.js
+++ EstructuraCine/tailwind.config.js
@@ -1,11 +1,14 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}',
-  'node_modules/flowbite-react/lib/esm/**/*.js'],
+export default {
+  content: [
+    // ...
+    'node_modules/flowbite-react/lib/esm/**/*.js'
+],
   theme: {
     extend: {},
   },
   plugins: [
     require('flowbite/plugin'),
-  ],
-};
+  ]
+}
+
