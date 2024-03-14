f2e7214eec3141841c2bae1f0a3d3f2e008c7049
edwingd18 <edwin.guerrero@uao.edu.co>
2024-03-13 23:40:12 -0500

    mejorando el componente

--- EstructuraCine/src/Components/Sidebar/Sidebar.jsx
+++ EstructuraCine/src/Components/Sidebar/Sidebar.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import { Sidebar as FlowbiteSidebar } from 'flowbite-react';
-import { HiArrowSmRight, HiChartPie, HiInbox, HiShoppingBag, HiTable, HiUser, HiViewBoards } from 'react-icons/hi';
+import { HiOutlineChatAlt2 , HiShoppingCart , HiUser,HiTicket  } from 'react-icons/hi';
 import './Sidebar.css'; // Importa tu archivo de estilos CSS para el Sidebar si lo tienes
 
 function Sidebar() {
@@ -22,31 +22,24 @@ function Sidebar() {
       onMouseEnter={openSidebar} // Abrir el Sidebar cuando el mouse entra
       onMouseLeave={closeSidebar} // Cerrar el Sidebar cuando el mouse sale
     >
-      <FlowbiteSidebar aria-label="Default sidebar example">
+      <FlowbiteSidebar aria-label="Menu de Cine" >
         <FlowbiteSidebar.Items>
-          <FlowbiteSidebar.ItemGroup>
-            <FlowbiteSidebar.Item href="#" icon={HiChartPie}>
-              Perfil
-            </FlowbiteSidebar.Item>
-            <FlowbiteSidebar.Item href="#" icon={HiViewBoards} label="Pro" labelColor="dark">
-              Pages
-            </FlowbiteSidebar.Item>
-            <FlowbiteSidebar.Item href="#" icon={HiInbox} label="3">
-              Inbox
-            </FlowbiteSidebar.Item>
-            <FlowbiteSidebar.Item href="#" icon={HiUser}>
-              Users
-            </FlowbiteSidebar.Item>
-            <FlowbiteSidebar.Item href="#" icon={HiShoppingBag}>
-              Products
-            </FlowbiteSidebar.Item>
-            <FlowbiteSidebar.Item href="#" icon={HiArrowSmRight}>
-              Sign In
-            </FlowbiteSidebar.Item>
-            <FlowbiteSidebar.Item href="#" icon={HiTable}>
-              Sign Up
-            </FlowbiteSidebar.Item>
-          </FlowbiteSidebar.ItemGroup>
+        <FlowbiteSidebar.ItemGroup>
+        <FlowbiteSidebar.Item href="#" icon={HiUser}>
+          Perfil
+        </FlowbiteSidebar.Item>
+        <FlowbiteSidebar.Item href="#" icon={HiTicket}>
+          Tiquetes
+        </FlowbiteSidebar.Item>
+        <FlowbiteSidebar.Item href="#" icon={HiShoppingCart}>
+          Carrito
+        </FlowbiteSidebar.Item>
+      </FlowbiteSidebar.ItemGroup>
+      <FlowbiteSidebar.ItemGroup>
+        <FlowbiteSidebar.Item href="#" icon={HiOutlineChatAlt2 }>
+          Mensajes
+        </FlowbiteSidebar.Item>
+      </FlowbiteSidebar.ItemGroup>
         </FlowbiteSidebar.Items>
       </FlowbiteSidebar>
     </div>
