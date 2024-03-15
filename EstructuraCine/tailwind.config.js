b98838a453bb3ce5f44f9670544373a45b389571
edwingd18 <edwin.guerrero@uao.edu.co>
2024-03-14 23:50:10 -0500

    estilos css de sidebar
    Faltan cambiar el color de  los iconos con el hover

--- EstructuraCine/tailwind.config.js
+++ EstructuraCine/tailwind.config.js
@@ -1,14 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
-    // ...
-    'node_modules/flowbite-react/lib/esm/**/*.js'
-],
-  theme: {
-    extend: {},
+    'node_modules/flowbite-react/lib/esm/**/*.js',
+    "./src/**/*.{js,jsx,ts,tsx}",
+],theme:{
+  extend: {
+    colors: {
+      'agua': '#181725',
+      'icon': '#E5E5EB'
+
+
+    },
   },
-  plugins: [
-    
-  ]
+},
+  plugins: [require('flowbite/plugin')],
 }
 
