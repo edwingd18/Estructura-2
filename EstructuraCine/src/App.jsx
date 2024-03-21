243a505988d6166dfc57b1113a9ccdc0825e1a5f
Juan Miguel León Gómez <85261235+Juanm0331123@users.noreply.github.com>
2024-03-20 21:09:14 -0500

    Se está realizando el codigo de comida. Mañana se continua con el diseño de la interfaz

--- EstructuraCine/src/App.jsx
+++ EstructuraCine/src/App.jsx
@@ -2,14 +2,16 @@ import { Routes, Route } from "react-router-dom";
 import Carousel from "./Components/Carousel/Carousel";
 import MovieInfo from "./Components/MovieInfo/MovieInfo";
 import { movies } from "./constants/moviesData";
-import SelectTickets from "./Components/SelectTickets - Part one/SelectTickets";
+// import SelectTickets from "./Components/SelectTickets - Part one/SelectTickets";
+import Food from "./Components/SelectFood/Food";
 
 const App = () => {
   return (
     <Routes>
       <Route path="/" element={<Carousel movies={movies} />} />
       <Route path="/movie/:id" element={<MovieInfo movies={movies} />} />
-      <Route path="/selecttickets" element={<SelectTickets />} />
+      {/* <Route path="/selecttickets" element={<SelectTickets />} /> */}
+      <Route path="/selectFood" element={<Food />} />
     </Routes>
   );
 };
--- EstructuraCine/src/main.jsx
+++ EstructuraCine/src/main.jsx
@@ -4,6 +4,7 @@ import "./index.css";
 import Sidebar from "./Components/Sidebar/Sidebar.jsx";
 import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
+import Food from "./Components/SelectFood/Food.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
@@ -11,5 +12,6 @@ ReactDOM.createRoot(document.getElementById("root")).render(
       <Sidebar />
       <App />
     </BrowserRouter>
+    <Food />
   </React.StrictMode>
 );
