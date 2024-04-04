634b04b0d2876d8dbb8b38ae6a33c1f238f34213
Juan Miguel León Gómez <85261235+Juanm0331123@users.noreply.github.com>
2024-04-03 21:58:39 -0500

    Token

--- Backend/middleware/token.JWT.js
+++ Backend/middleware/token.JWT.js
@@ -1,5 +1,5 @@
 export const verifyToken = (req, res, next) => {
-    const token = req.headers.authorization?.split(' ')[1];
+    const token = req.headers['authorization'];
 
     if (!token) {
         return res.status(401).json({ error: 'Token no proporcionado' });
