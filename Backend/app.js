d17c1dc81552c576c56cc9ae54a01a4b75359660
Cristian Camilo Domínguez Gutiérrez <147455398+CristianCDG@users.noreply.github.com>
2024-04-06 09:58:42 -0500

    New User Creation

--- Backend/app.js
+++ Backend/app.js
@@ -11,6 +11,7 @@ app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use('/movies', MovieRoutes)
+app.use('/users', userRoutes);
 
 //Ruta de bienvenida
 app.listen(8000, () => {
