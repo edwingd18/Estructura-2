304ae81ebae9b5e1ce702fec1285d9b633216aac
Cristian Camilo Domínguez Gutiérrez <147455398+CristianCDG@users.noreply.github.com>
2024-04-22 22:45:52 -0500

    Implemented Seat IDS for seat selection

--- Frontend/src/Components/SelectTickets - Part one/SelectChair.jsx
+++ Frontend/src/Components/SelectTickets - Part one/SelectChair.jsx
@@ -1,10 +1,19 @@
 import '../SelectTickets - Part one/SelectChair.css'
 import PropTypes from 'prop-types';
+import React, { useRef } from 'react';
 
 import imagenSillaGeneral from '../SelectTickets - Part one/imagenes-sillas/Silla-general.png'
 import imagenSillaPreferencial from '../SelectTickets - Part one/imagenes-sillas/Silla-preferencial.png'
 
 export function SelectChair(props) {
+    const genComboRef = useRef();
+
+    const handleSelectionChange = () => {
+        if (props.onSelectionChange){
+            props.onSelectionChange(genComboRef.current.value);
+        }
+    };
+
     return (
         <div className='contenedor-silla'>
             <div className="titulo-silla">
@@ -22,16 +31,16 @@ export function SelectChair(props) {
                         <h3>Valor ${props.precio}</h3>
                     </div>
                     <div className="cantidad-sillas">
-                        <select>
+                        <select id="genCombo" ref={genComboRef} onChange={handleSelectionChange}>
                             <option value="0">0</option>
                             <option value="1">1</option>
                             <option value="2">2</option>
                             <option value="3">3</option>
                             <option value="4">4</option>
                             <option value="5">5</option>
-                            <option value="1">6</option>
-                            <option value="2">7</option>
-                            <option value="3">8</option>
+                            <option value="6">6</option>
+                            <option value="7">7</option>
+                            <option value="8">8</option>
                         </select>
                     </div>
                 </div>
@@ -41,6 +50,13 @@ export function SelectChair(props) {
 }
 
 export function SelectChairPreferencial(props) {
+    const prefComboRef = useRef();
+
+    const handleSelectionChange = () => {
+        if (props.onSelectionChange){
+            props.onSelectionChange(prefComboRef.current.value);
+        }
+    };
     return (
         <div className='contenedor-silla'>
             <div className="titulo-silla">
@@ -58,16 +74,16 @@ export function SelectChairPreferencial(props) {
                         <h3>Valor ${props.precio}</h3>
                     </div>
                     <div className="cantidad-sillas">
-                        <select>
+                        <select id="prefCombo" ref={prefComboRef} onChange={handleSelectionChange}>
                             <option value="0">0</option>
                             <option value="1">1</option>
                             <option value="2">2</option>
                             <option value="3">3</option>
                             <option value="4">4</option>
                             <option value="5">5</option>
-                            <option value="1">6</option>
-                            <option value="2">7</option>
-                            <option value="3">8</option>
+                            <option value="6">6</option>
+                            <option value="7">7</option>
+                            <option value="8">8</option>
                         </select>
                     </div>
                 </div>
@@ -80,10 +96,12 @@ SelectChair.propTypes = {
     nombre: PropTypes.string.isRequired,
     nombreTipo: PropTypes.string.isRequired,
     precio: PropTypes.string.isRequired,
+    onSelectionChange: PropTypes.func,
 };
 
 SelectChairPreferencial.propTypes = {
     nombre: PropTypes.string.isRequired,
     nombreTipo: PropTypes.string.isRequired,
     precio: PropTypes.string.isRequired,
+    onSelectionChange: PropTypes.func,
 };
